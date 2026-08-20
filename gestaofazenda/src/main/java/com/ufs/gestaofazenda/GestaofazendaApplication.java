93f876996267c99d2a1f32e986957360f394414c
Gustaahhh <gustavocsilva117@gmail.com>
2026-08-19 21:22:19 -0300

    primeira commit

---
+++ gestaofazenda/src/main/java/com/ufs/gestaofazenda/GestaofazendaApplication.java
@@ -0,0 +1,13 @@
+package com.ufs.gestaofazenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaofazendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaofazendaApplication.class, args);
+	}
+
+}
---
+++ gestaofazenda/src/test/java/com/ufs/gestaofazenda/GestaofazendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufs.gestaofazenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaofazendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
