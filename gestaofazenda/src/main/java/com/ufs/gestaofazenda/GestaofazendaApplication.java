86c86b7a2a0c07f1041dc8190c2bb0d2d7b8621a
Marcos Antônio <marcosantoniosantossouza13@gmail.com>
2026-08-19 22:45:17 -0300

    pra eu ser contribuidor

--- gestaofazenda/src/main/java/com/ufs/gestaofazenda/GestaofazendaApplication.java
+++ gestaofazenda/src/main/java/com/ufs/gestaofazenda/GestaofazendaApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GestaofazendaApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(GestaofazendaApplication.class, args);
 	}
-
 }
